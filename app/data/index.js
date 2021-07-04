19b4404d9b3967cf9e43f10f87cd68d376cf3ee2
lemamon <demmarinho@gmail.com>
2021-07-04 02:55:49 -0300

    feat: create data

---
+++ app/data/index.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: "0",
+    uri: "https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2021/05/leonardo-da-vinci.jpeg",
+    name: "Leonardo da Vinci",
+    resume: "Artist, sculler, architect and inventor",
+    location: "Republic of Florence 1452-1519",
+  },
+  {
+    id: "1",
+    uri: "https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2021/05/leonardo-da-vinci.jpeg",
+    name: "Leonardo da Vinci",
+    resume: "Artist, sculler, architect and inventor",
+    location: "Republic of Florence 1452-1519",
+  },
+  {
+    id: "2",
+    uri: "https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2021/05/leonardo-da-vinci.jpeg",
+    name: "Leonardo da Vinci",
+    resume: "Artist, sculler, architect and inventor",
+    location: "Republic of Florence 1452-1519",
+  },
+  {
+    id: "3",
+    uri: "https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2021/05/leonardo-da-vinci.jpeg",
+    name: "Leonardo da Vinci",
+    resume: "Artist, sculler, architect and inventor",
+    location: "Republic of Florence 1452-1519",
+  },
+];
+
+export default data;
