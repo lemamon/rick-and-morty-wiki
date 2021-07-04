7d362a1ccb50859c82c641f088e984f8da8b472a
lemamon <demmarinho@gmail.com>
2021-07-04 15:53:32 -0300

    fix: added detils

--- app/App.js
+++ app/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import data from "./data";
+import Detail from "./screens/Detail";
 import Home from "./screens/Home";
 
 export default function App() {
-  return <Home />;
+  // return <Home />;
+  return <Detail card={data[0]} />;
 }
