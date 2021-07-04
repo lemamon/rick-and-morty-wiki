7dc68fda7dad53185e3adbfe3aca1a370bebc4a8
lemamon <demmarinho@gmail.com>
2021-07-04 02:55:39 -0300

    feat: added card component

---
+++ app/components/Card.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  letter-spacing: 1px;
+  margin: 0 10px 40px;
+`;
+
+const Image = styled.Image`
+  width: 300px;
+  height: 180px;
+  border-radius: 15px;
+  box-shadow: rgba(0, 0, 0, 0.24) 10px 20px 10px;
+  margin: 40px 0 20px 0;
+`;
+
+const Title = styled.Text`
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+const SubTitle = styled.Text`
+  font-size: 12px;
+  line-height: 20px;
+`;
+
+function Card({ uri, name, resume, location }) {
+  return (
+    <Container>
+      <Image source={{ uri }} />
+      <Title>{name}</Title>
+      <SubTitle>{resume}</SubTitle>
+      <SubTitle>{location}</SubTitle>
+    </Container>
+  );
+}
+
+export default Card;
