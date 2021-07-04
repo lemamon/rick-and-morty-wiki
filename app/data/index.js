50456afe1be6ddb16c45409e43d95bd7cb406029
lemamon <demmarinho@gmail.com>
2021-07-04 14:14:42 -0300

    fix: added one more to title

--- app/data/index.js
+++ app/data/index.js
@@ -27,6 +27,13 @@ const data = [
     resume: "Artist, sculler, architect and inventor",
     location: "Republic of Florence 1452-1519",
   },
+  {
+    id: "",
+    uri: "https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2021/05/leonardo-da-vinci.jpeg",
+    name: "Leonardo da Vinci",
+    resume: "Artist, sculler, architect and inventor",
+    location: "Republic of Florence 1452-1519",
+  },
 ];
 
 export default data;
